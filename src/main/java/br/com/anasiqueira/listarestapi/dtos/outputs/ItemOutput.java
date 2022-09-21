2ed109931a3420d9755b071570de920c6041e9ab
Ana Siqueira <ana.siqueira@gpnet.com.br>
2022-09-21 13:50:03 -0300

    Finalização da nomeclatura de Input, Output e Entity

--- src/main/java/br/com/anasiqueira/listarestapi/dtos/outputs/ItemOutput.java
+++ src/main/java/br/com/anasiqueira/listarestapi/dtos/outputs/ItemOutput.java
@@ -1,5 +1,16 @@
 package br.com.anasiqueira.listarestapi.dtos.outputs;
 
+import java.util.List;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ItemOutput {
 
+	private Long id;
+	private String nomeItem;
+	private List<ListaOutput> listas;
+	
 }
--- src/main/java/br/com/anasiqueira/listarestapi/entities/ItemEntity.java
+++ src/main/java/br/com/anasiqueira/listarestapi/entities/ItemEntity.java
@@ -1,5 +1,35 @@
 package br.com.anasiqueira.listarestapi.entities;
 
-public class ItemEntity {
+import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_itens")
+@Getter
+@Setter
+public class ItemEntity {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(length = 100, name = "itemLista")
+	private String itemLista;
+	
+	@ManyToOne
+	@JoinTable(name = "tb_itens_listas", joinColumns = @JoinColumn(name = "item_id", referencedColumnName = "id"), inverseJoinColumns = @JoinColumn(name = "lista_id", referencedColumnName = "id"))
+	private List<ListaEntity> listas;
+	
 }
